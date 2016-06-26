f50c9e0bf955d561536ad0c2eca0e1232cf3c324
Daniel Tschan <tschan@puzzle.ch>
2016-06-26 20:59:52 +0200

    Adapt to Docker build strategy.

--- vmbuild.sh
+++ vmbuild.sh
@@ -8,7 +8,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 	EOF
   (cd /; tar cf - tmp/build.sh run/secrets)
   cat <<-EOF
-		trap 'rm -rf /run/secrets' EXIT
+		# trap 'rm -rf /run/secrets' EXIT
 
 		`export -p`
 
