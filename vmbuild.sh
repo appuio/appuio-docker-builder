411bd68ccc084d4fd14db3d4d7d660d3c4aec133
Daniel Tschan <tschan@puzzle.ch>
2016-06-26 20:23:32 +0200

    Adapt to Docker build strategy.

--- vmbuild.sh
+++ vmbuild.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 {
   cat <<-EOF
 		(cd / && tar xf -)
@@ -10,4 +14,4 @@
 
 		/tmp/build.sh
 	EOF
-} | ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -R 5000:172.30.1.1:5000 builder@buildvm1.beta.puzzle.cust.vshn.net ssh -R 5000:localhost:5000 default sudo bash
+} | ${SCRIPT_DIR}/vmconnect.sh
